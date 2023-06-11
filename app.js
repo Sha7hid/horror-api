66185a0b505d3e7d46fc6cd5957173e4ed9cc53f
sha7hid <shahidshafadshahad@gmail.com>
2023-06-11 19:36:23 +0530

    cors add

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(cors(corsOptions));
 const connectDB = require("./config/db");
 
 const mongo_uri = `mongodb+srv://shahid:arthur#540913@cluster0.ggcnvuy.mongodb.net/film-api`;
